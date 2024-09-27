b0efa049d0f2a388ec2a54bad4c790e8ac9d8950
tommysghid <tpeterson@stpsworld.com>
2024-09-27 14:57:39 -0400

    Update database.php

--- config/database.php
+++ config/database.php
@@ -5,7 +5,7 @@ global $CFG;
 $CFG = new stdClass();
 
 $CFG->dbhost    = '127.0.0.1';
-$CFG->dbname    = 'pe';
-$CFG->dbuser    = 'test';
-$CFG->dbpassword = '76df9cA4c332e@eb';
-$CFG->dbport    = '3302';
+$CFG->dbname    = '';
+$CFG->dbuser    = '';
+$CFG->dbpassword = '';
+$CFG->dbport    = '';
